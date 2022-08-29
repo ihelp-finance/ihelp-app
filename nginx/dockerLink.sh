cb1849c0a385ecd2e6164d795186a391df2a97ee
turbinex-admin <ms@turbinex.io>
2022-08-29 18:22:14 +0000

    Updating dev env files.

--- nginx/dockerLink.sh
+++ nginx/dockerLink.sh
@@ -3,6 +3,6 @@
 rm /etc/nginx/sites-enabled/default > /dev/null 2>&1
 
 #ln /core/nginx/docker_nginx.conf /etc/nginx/sites-enabled/default
-cp /core/nginx/docker_nginx.conf /etc/nginx/nginx.conf
+cp /core/ihelp-app/nginx/docker_nginx.conf /etc/nginx/nginx.conf
 
 service nginx restart
